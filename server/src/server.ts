509790e4965e8a31b4be0c5cf0554e7f32744da9
Joaoojohn <joaopedro.cs12@hotmail.com>
2022-11-01 00:18:14 -0300

    teste commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({
   log: ["query"],
 });
-
+//teste
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
